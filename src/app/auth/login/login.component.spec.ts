fe0751b149c4186cff19f5a8bbb8df4eb7db4bb6
The4thLaw <The4thLaw@users.noreply.github.com>
2019-03-05 15:31:46 +0100

    #1: Fixing absolute imports

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -2,8 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AppRoutingModule } from 'src/app/app-routing.module';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
+import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
